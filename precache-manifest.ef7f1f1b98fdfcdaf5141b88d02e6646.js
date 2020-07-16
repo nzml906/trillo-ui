d691781bf3a04c44cbb14d29fc331670c911868d
Nazmul <grey000906@gmail.com>
2020-07-17 04:53:38 +0600

    deploy portfolio

---
+++ precache-manifest.ef7f1f1b98fdfcdaf5141b88d02e6646.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "106676d1a4e586345a49",
+    "url": "/trillo-ui/css/app.05431a9c.css"
+  },
+  {
+    "revision": "84aa053b49b1cb0d0c65783f5241c271",
+    "url": "/trillo-ui/img/SVG/aircraft-take-off.svg"
+  },
+  {
+    "revision": "b1e358dac9f6ed93b77e3a26554601a6",
+    "url": "/trillo-ui/img/SVG/bookmark.svg"
+  },
+  {
+    "revision": "807994987f885922725b32722b8ec357",
+    "url": "/trillo-ui/img/SVG/chat.svg"
+  },
+  {
+    "revision": "3574fb442c7fcc0ae147ddb5e0a30757",
+    "url": "/trillo-ui/img/SVG/chevron-thin-right.svg"
+  },
+  {
+    "revision": "03f24c33dacb18eeaba38414ff6f2b94",
+    "url": "/trillo-ui/img/SVG/home.svg"
+  },
+  {
+    "revision": "ee4314ab0030690bbbddff6bcf9c2e11",
+    "url": "/trillo-ui/img/SVG/key.svg"
+  },
+  {
+    "revision": "d6337acc65d95ea9c6b73635de5acec2",
+    "url": "/trillo-ui/img/SVG/location-pin.svg"
+  },
+  {
+    "revision": "09bf8ae3ac2fcd070e04781738e4bea0",
+    "url": "/trillo-ui/img/SVG/magnifying-glass.svg"
+  },
+  {
+    "revision": "d5a4f7e186267e097a12f5ee28aff571",
+    "url": "/trillo-ui/img/SVG/map.svg"
+  },
+  {
+    "revision": "1f1e3e5583346a60ec12ffcc12486af8",
+    "url": "/trillo-ui/img/SVG/star.svg"
+  },
+  {
+    "revision": "6f98acff3163c6603e8a52b1e97e3f7a",
+    "url": "/trillo-ui/img/chevron-thin-right.6f98acff.svg"
+  },
+  {
+    "revision": "6f98acff3163c6603e8a52b1e97e3f7a",
+    "url": "/trillo-ui/img/chevron-thin-right.svg"
+  },
+  {
+    "revision": "976699dbfc219bca44b2bc62cfcdb53f",
+    "url": "/trillo-ui/img/favicon.png"
+  },
+  {
+    "revision": "5a550a3da0d63bfa7ec73ac1f1acdbba",
+    "url": "/trillo-ui/img/hotel-1.jpg"
+  },
+  {
+    "revision": "5468a3b162abcbb501141ce0e7f4aa6e",
+    "url": "/trillo-ui/img/hotel-2.jpg"
+  },
+  {
+    "revision": "7af5efbc95e0464867f4327aee208ad5",
+    "url": "/trillo-ui/img/hotel-3.jpg"
+  },
+  {
+    "revision": "8feba60219eee59451ba4563cce9a002",
+    "url": "/trillo-ui/img/logo.png"
+  },
+  {
+    "revision": "79514b076fb1acd082b93539344d3650",
+    "url": "/trillo-ui/img/sprite.svg"
+  },
+  {
+    "revision": "cc55935517ae3cd739420eed530dae31",
+    "url": "/trillo-ui/img/text.txt"
+  },
+  {
+    "revision": "f025345a00d65987a9a2f813c54c0611",
+    "url": "/trillo-ui/img/user-1.jpg"
+  },
+  {
+    "revision": "9ae2a1e2c402759e939fc705c3f613d2",
+    "url": "/trillo-ui/img/user-2.jpg"
+  },
+  {
+    "revision": "1eeec19af2564ccab82ee1622e54a34f",
+    "url": "/trillo-ui/img/user-3.jpg"
+  },
+  {
+    "revision": "b779eca12905edb571bcf6ddaad660b5",
+    "url": "/trillo-ui/img/user-4.jpg"
+  },
+  {
+    "revision": "7d6ca7d282e0b14ff8fc5c56832ac910",
+    "url": "/trillo-ui/img/user-5.jpg"
+  },
+  {
+    "revision": "cf523967089ecc2735e2ed3b045fe938",
+    "url": "/trillo-ui/img/user-6.jpg"
+  },
+  {
+    "revision": "a2a26d7906c257d584688e4a0130fe8c",
+    "url": "/trillo-ui/img/user.jpg"
+  },
+  {
+    "revision": "37a2eef84619e9062853684d0a68d825",
+    "url": "/trillo-ui/index.html"
+  },
+  {
+    "revision": "106676d1a4e586345a49",
+    "url": "/trillo-ui/js/app.49ea617a.js"
+  },
+  {
+    "revision": "9e83726d6b1a868d032f",
+    "url": "/trillo-ui/js/chunk-vendors.bdc9172a.js"
+  },
+  {
+    "revision": "758b69ecbcb44adcb89e36bd823cdb76",
+    "url": "/trillo-ui/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/trillo-ui/robots.txt"
+  }
+]);
